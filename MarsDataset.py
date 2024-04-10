c4c2e3475dca75362042ff40c9f9b42d3fa18ad1
kbade <bade.katharina@gmail.com>
2024-04-10 09:53:30 +0200

    format files

--- MarsDataset.py
+++ MarsDataset.py
@@ -1,88 +1,86 @@
-
 import numpy as np
 import torch
 import zarr
 import pandas as pd
 import code
 
+
 ###################################################
 class MarsDataset(torch.utils.data.IterableDataset):
 
-  #####################
-  def __init__(self, path_file, batch_size):
-    
-    super(MarsDataset, self).__init__()
+    #####################
+    def __init__(self, path_file, batch_size):
 
-    self.batch_size = batch_size
+        super(MarsDataset, self).__init__()
 
-    store = zarr.DirectoryStore(path_file)
-    self.sources = zarr.group(store=store)
+        self.batch_size = batch_size
 
-    self.rng = np.random.default_rng()
-    self.shuffle()
-  
-  #####################
-  def shuffle(self) :
+        store = zarr.DirectoryStore(path_file)
+        self.sources = zarr.group(store=store)
 
-    len = self.sources['time'].shape[0]
-    self.idxs = self.rng.permutation( np.arange(len))
-    
-    self.len = self.idxs.shape[0]
-    
-  #####################
-  def __len__(self):
-    return self.len
+        self.rng = np.random.default_rng()
+        self.shuffle()
 
-  #####################
-  def __iter__(self):
+    #####################
+    def shuffle(self):
 
-    self.shuffle()
-    iter_start, iter_end = self.worker_workset()
+        len = self.sources['time'].shape[0]
+        self.idxs = self.rng.permutation(np.arange(len))
 
-    for bidx in range( iter_start, iter_end, self.batch_size) :
+        self.len = self.idxs.shape[0]
 
-      idx_t = self.idxs[bidx : bidx+self.batch_size]
+    #####################
+    def __len__(self):
+        return self.len
 
-      source = torch.stack([
-        torch.tensor(self.sources['temp'][idx_t]),
-        torch.tensor(self.sources['u'][idx_t]),
-        torch.tensor(self.sources['v'][idx_t])
-      ], 1)
+    #####################
+    def __iter__(self):
 
-      # target is subsequent time step
-      idx_t += 1
-      target = torch.stack([
-        torch.tensor(self.sources['temp'][idx_t]),
-        torch.tensor(self.sources['u'][idx_t]),
-        torch.tensor(self.sources['v'][idx_t])
-      ], 1)
-      
-      # TODO: data normalization
-      
-      yield (source, target)
+        self.shuffle()
+        iter_start, iter_end = self.worker_workset()
 
-  #####################
-  def __len__(self):
-      return self.len // self.batch_size
+        for bidx in range(iter_start, iter_end, self.batch_size):
+            idx_t = self.idxs[bidx: bidx + self.batch_size]
 
-  #####################
-  def worker_workset( self) :
+            source = torch.stack([
+                torch.tensor(self.sources['temp'][idx_t]),
+                torch.tensor(self.sources['u'][idx_t]),
+                torch.tensor(self.sources['v'][idx_t])
+            ], 1)
 
-    worker_info = torch.utils.data.get_worker_info()
+            # target is subsequent time step
+            idx_t += 1
+            target = torch.stack([
+                torch.tensor(self.sources['temp'][idx_t]),
+                torch.tensor(self.sources['u'][idx_t]),
+                torch.tensor(self.sources['v'][idx_t])
+            ], 1)
 
-    if worker_info is None: 
-      iter_start = 0
-      iter_end = len(self)
+            # TODO: data normalization
 
-    else:  
-      # split workload
-      temp = len(self)
-      per_worker = int( np.floor( temp / float(worker_info.num_workers) ) )
-      worker_id = worker_info.id
-      iter_start = int(worker_id * per_worker)
-      iter_end = int(iter_start + per_worker)
-      if worker_info.id+1 == worker_info.num_workers :
-        iter_end = int(temp)
+            yield (source, target)
 
-    return iter_start, iter_end
-  
+    #####################
+    def __len__(self):
+        return self.len // self.batch_size
+
+    #####################
+    def worker_workset(self):
+
+        worker_info = torch.utils.data.get_worker_info()
+
+        if worker_info is None:
+            iter_start = 0
+            iter_end = len(self)
+
+        else:
+            # split workload
+            temp = len(self)
+            per_worker = int(np.floor(temp / float(worker_info.num_workers)))
+            worker_id = worker_info.id
+            iter_start = int(worker_id * per_worker)
+            iter_end = int(iter_start + per_worker)
+            if worker_info.id + 1 == worker_info.num_workers:
+                iter_end = int(temp)
+
+        return iter_start, iter_end
--- tests.py
+++ tests.py
@@ -5,21 +5,19 @@ marsdata = MarsDataset('data/test.zarr', 32)
 
 dataset_iter = iter(marsdata)
 
-a = next(dataset_iter)
+a = next(dataset_iter)  # zarr_array.oindex([])
 
 # print(a[0].shape)
 
 torch.Size([32, 3, 70, 36, 72])
 
+loader_params = {'batch_size': None, 'batch_sampler': None, 'shuffle': False, 'num_workers': 1, 'pin_memory': True}
 
-loader_params = { 'batch_size': None, 'batch_sampler': None, 'shuffle': False,  'num_workers': 1, 'pin_memory': True }
-
-data_loader = torch.utils.data.DataLoader(marsdata, **loader_params, sampler = None) 
+data_loader = torch.utils.data.DataLoader(marsdata, **loader_params, sampler=None)
 
 data_loader_iter = iter(data_loader)
 
-
-for bidx, data in enumerate(dataset_iter) :
+for bidx, data in enumerate(dataset_iter):
     print(bidx)
     print(data)
-  # feed data to network
+# feed data to network
