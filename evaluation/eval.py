c91aa68732149dbc99bd8965a73287c050ab6436
Knulon <marlon.hoerner.02@gmail.com>
2024-04-25 10:50:52 +0200

    Add Eval option for Denormalized Data Output

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -10,6 +10,7 @@ CHECKPOINT_PATH = os.environ.get("PATH_CHECKPOINT", "../model/")
 LEVEL_FROM_BOTTOM = 5
 AVAIL_GPUS = min(1, torch.cuda.device_count())
 BATCH_SIZE = 256 if AVAIL_GPUS else 64
+NORMALIZATION = False
 
 model = UNet(level=LEVEL_FROM_BOTTOM * 3)
 model.load_state_dict(torch.load(CHECKPOINT_PATH + "new_unet.pt"))
@@ -21,18 +22,31 @@ count = 1
 for e in dataset:
     print("Printing ", count, "...")
 
-    batch = e[0][0].transpose(0, 1).transpose(1, 2)
-    batch = np.reshape(batch.detach(), (36, 72, 3, LEVEL_FROM_BOTTOM))
-    heat_plotting(create_denormalized_matrix_from_tensor(batch, LEVEL_FROM_BOTTOM), "e0-model-1" + str(count), 0, 0)
+    if NORMALIZATION:
+        batch = e[0][0].transpose(0, 1).transpose(1, 2)
+        batch = np.reshape(batch.detach(), (36, 72, 3, LEVEL_FROM_BOTTOM))
+    else:
+        batch = create_denormalized_matrix_from_tensor(e[0][0], LEVEL_FROM_BOTTOM)
 
-    batch = e[1][0].transpose(0, 1).transpose(1, 2)
-    batch = np.reshape(batch.detach(), (36, 72, 3, LEVEL_FROM_BOTTOM))
-    heat_plotting(create_denormalized_matrix_from_tensor(batch, LEVEL_FROM_BOTTOM), "e1-model-1" + str(count), 0, 0)
+    heat_plotting(batch, "e0-model-1" + str(count), 0, 0)
+
+    if NORMALIZATION:
+        batch = e[1][0].transpose(0, 1).transpose(1, 2)
+        batch = np.reshape(batch.detach(), (36, 72, 3, LEVEL_FROM_BOTTOM))
+    else:
+        batch = create_denormalized_matrix_from_tensor(e[1][0], LEVEL_FROM_BOTTOM)
+
+    heat_plotting(batch, "e1-model-1" + str(count), 0, 0)
 
     prediction = model(e[0])
-    batch = prediction[0].transpose(0, 1).transpose(1, 2)
-    batch = np.reshape(batch.detach(), (36, 72, 3, LEVEL_FROM_BOTTOM))
-    heat_plotting(create_denormalized_matrix_from_tensor(batch, LEVEL_FROM_BOTTOM), "p-model-1" + str(count), 0, 0)
+
+    if NORMALIZATION:
+        batch = prediction[0].transpose(0, 1).transpose(1, 2)
+        batch = np.reshape(batch.detach(), (36, 72, 3, LEVEL_FROM_BOTTOM))
+    else:
+        batch = create_denormalized_matrix_from_tensor(prediction[0].detach(), LEVEL_FROM_BOTTOM)
+
+    heat_plotting(batch, "p-model-1" + str(count), 0, 0)
     count += 1
     if count == 6:
         break
