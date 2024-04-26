f34a6fab465f5ce89e305910e280d76de3d0a405
Knulon <marlon.hoerner.02@gmail.com>
2024-04-26 14:58:00 +0200

    add netoworks and lossprint

---
+++ evaluation/lossprint.py
@@ -0,0 +1,5 @@
+from visualizations.visualisation import loss_or_acc_plotting
+
+loss = [0.0012, 0.0006, 0.0004, 0.0003, 0.0002, 0.0001, 0.0001, 0.0002, 0.0002, 0.0002]
+
+loss_or_acc_plotting(loss, "MSE Loss")
