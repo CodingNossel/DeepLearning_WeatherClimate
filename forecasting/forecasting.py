2f6c6f3298db1b73ac0d9ff11be635fa9d9f48bc
Knulon <marlon.hoerner.02@gmail.com>
2024-05-02 18:23:06 +0200

    MarsDataset and forecast cleanup

--- forecasting/forecasting.py
+++ forecasting/forecasting.py
@@ -6,24 +6,24 @@ from unet.unet import UNet
 from visualizations.visualisation import heat_plotting
 
 CHECKPOINT_PATH = os.environ.get("PATH_CHECKPOINT", "../model/")
-LEVEL_FROM_BOTTOM = 5
+LEVEL_FROM_BOTTOM = 10
 AVAIL_GPUS = min(1, torch.cuda.device_count())
 BATCH_SIZE = 256 if AVAIL_GPUS else 64
 
 model = UNet(level=LEVEL_FROM_BOTTOM * 3)
-model.load_state_dict(torch.load(CHECKPOINT_PATH + "new_unet.pt"))
+model.load_state_dict(torch.load(CHECKPOINT_PATH + "network_64B_10L_15E_1080T_0.01R_SL1.pt"))
 model.eval()
 
 
 @torch.no_grad()
-def forecast(currentState, hours=2):
+def forecast(currentState, levels, hours=2):
     """
     Forecasting a weather state.
     :param currentState: current state of the weather
     :param hours: the state of in how many hours shall be predicted; must be dividable by 2
     :return: predicted state of the weather in given hours
     """
-    pred = torch.zeros(64, 15, 36, 72)
+    pred = torch.zeros(64, levels*3, 36, 72)
     pred[0] = currentState
     for i in range(int(hours / 2)):
         print("Predicting", i*2+2, "...")
--- tests/forecast.py
+++ tests/forecast.py
@@ -1,8 +1,8 @@
 from dataloader.MarsDataset import MarsDataset
 from forecasting.forecasting import forecast
 
-dataset = MarsDataset(path_file="../data/my27.zarr", batch_size=1, level_from_bottom=5)
+dataset = MarsDataset(path_file="../data/my27.zarr", batch_size=1, level_from_bottom=10)
 
 for e in dataset:
-    forecast(e[0][0], 6)
+    forecast(e[0][0], 10, 6)
     exit()
